8f92a2fcfc2f5cd596eead38de2d7d317cbb74aa
ZHANGW31 <wei.zhang4@outlook.com>
2020-10-07 15:00:08 -0700

    added initRoom method

--- src/com/capstone/InitXML.java
+++ src/com/capstone/InitXML.java
@@ -62,4 +62,37 @@ public class InitXML {
         }
 
     }
+    public void initRooms() {
+        try {
+
+            //big formating block for taking XML from the provided txt doc "NPCs.txt" in data
+            File inputFile = new File(String.valueOf(Path.of("data", "Rooms.txt")));
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList roomList = doc.getElementsByTagName("room"); //npcList now contains all the nodes in the file with tag NPC
+
+            for (int temp = 0; temp < roomList.getLength(); temp++) { //iterating over all the nodes in npcList
+
+                Node room = roomList.item(temp);
+
+                //downcasting, getting element we want, then recasting to node, then getting the text .... what
+                Element roomEle = (Element) room;
+                String roomName = roomEle.getElementsByTagName("name").item(0).getTextContent();
+                String roomDescription = roomEle.getElementsByTagName("description").item(0).getTextContent();
+                String roomAdjNorth = roomEle.getElementsByTagName("adjacent_north").item(0).getTextContent();
+                String roomAdjSouth = roomEle.getElementsByTagName("adjacent_south").item(0).getTextContent();
+                String roomAdjEast = roomEle.getElementsByTagName("adjacent_east").item(0).getTextContent();
+                String roomAdjWest = roomEle.getElementsByTagName("adjacent_west").item(0).getTextContent();
+
+                listOfRooms.add(new Room(roomName,roomDescription,roomAdjNorth,roomAdjSouth,roomAdjEast,roomAdjWest));
+
+            }
+        }
+        catch (Exception e) {
+            System.out.println("there was an error");
+        }
+
+    }
 }

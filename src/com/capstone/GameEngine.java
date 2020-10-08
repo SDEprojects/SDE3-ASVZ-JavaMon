13ccfcc9ff507dcbaa607307de1df61486f2b00d
yunyidi-han <yunyidi.han@gmail.com>
2020-10-08 12:58:36 -0700

    Modify emeke parser

--- src/com/capstone/GameEngine.java
+++ src/com/capstone/GameEngine.java
@@ -7,6 +7,52 @@ public class GameEngine {
     public static void main(String[] args) {
         InitXML game = new InitXML();
         game.initNPCs();
+        game.initRooms();
+        Player player1 = new Player("Subash");
+        Room startingRoom = game.getRoom("Oak's Lab");
+        player1.setCurrentRoom(startingRoom);
+
+        //actual loop for gamez
+        while (true) {
+            //promp user for input
+            player1.getCurrentRoom().displayOutput();
+            String userInput = "What do you wanna do? ";
+
+            //for the movement
+            if (userInput == "move or something") {
+                String direction = "some user input direction";
+                if (player1.validMove(direction)) ;
+                switch (direction) {
+                    case "north":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getNorthTile()));
+                    case "east":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getEastTile()));
+                    case "south":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getSouthTile()));
+                    case "West":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getWestTile()));
+                }
+            }
+            else if (userInput == "interact ") {
+                String interact = "some user interact object";
+                if (player1.validMove(direction));
+                switch (direction) {
+                    case "north":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getNorthTile()));
+                    case "east":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getEastTile()));
+                    case "south":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getSouthTile()));
+                    case "West":
+                        player1.setCurrentRoom(game.getRoom(player1.getCurrentRoom().getWestTile()));
+                }
+            }
+
+        }
+
+
+
+        //everything below is hardcoded for testing
 
         //for getting dialog of a npc, do these calls...
         System.out.println(game.listOfNpcs);
@@ -14,11 +60,28 @@ public class GameEngine {
 
 
         //must setCurrentRoom(room) first before calling move and other stuff)
-        Player player1 = new Player("Subash");
+
+        System.out.println(player1.getName());
 
 
-        //for moving rooms, call player.validMove(direction) for the boolean, and if true... then do player.setCurrentRoom(room)
 
+        startingRoom.displayOutput();
+
+        //System.out.println(player1.getCurrentRoom()); //room class needs a toString to display itself... if needed, prob not needed
+        System.out.println("===============================================");
+
+        if (player1.validMove("north")) {
+            System.out.println("PLayer moved north");
+            System.out.println("===============================================");
+            Room roomNorth = game.getRoom(player1.getCurrentRoom().getNorthTile());
+            player1.setCurrentRoom(roomNorth);
+            roomNorth.displayOutput();
+
+
+            //ASK TOM.... For implementation of NPCs in a Room... should we do a HAS-A relationship, or just query NPC Dialog when you interact as long as the current room contains the NPC.
+
+
+        }
     }
 
 }
--- src/com/capstone/TextParser.java
+++ src/com/capstone/TextParser.java
@@ -5,46 +5,67 @@ import java.util.Scanner;
 public class TextParser {
     private String userInput;
 
-    public static void main(String[] args) {
-        // An instance of the TextParser class can be instantiated to call non static method
-        // TextParser parse = new TextParser();
-        isValidString();
-    }
-
-    private static String isValidString() {
+    public String getUserInput() {
         String inputString = null;
         try {
             System.out.println("Enter a String: ");
             Scanner scanner = new Scanner(System.in);
             inputString = scanner.nextLine();
 
+            //input validation
+            if (!inputValidation(inputString)) {
+                inputValidation(inputString);
+            }
+
+
             /* Future work: create a holder for valid moves to be parsed in and
             verify presence in holder using less conditionals.
             This will make a more robust and less run-time demanding code*/
 
-            // conditions validate input parsed by scanner class above from the console
-            if (inputString.isEmpty() ) {
-                System.out.println("Your input cannot be empty");
-                isValidString();
-                //throw new IllegalArgumentException("You have not entered a move");
-            }else {
-                boolean numTrue = inputString.matches("[-+]?[0-9]*\\.?[0-9]+");
-                if (numTrue) {
-                    System.out.println("You have entered an invalid move");
-                    isValidString();
-                }
+//            // conditions validate input parsed by scanner class above from the console
+//            if (inputString.isEmpty() ) {
+//                System.out.println("Your input cannot be empty");
+//                isValidString();
+//                //throw new IllegalArgumentException("You have not entered a move");
+//            }
+//            else if (inputString.matches("[-+]?[0-9]*\\.?[0-9]+")) {
+//                System.out.println("You have entered an invalid move");
+//                isValidString();
+//            }
+
+            //conditions for actual action
+            if (inputString.split(" ")[0].toLowerCase().equals("go")) {
+
             }
+            else if (inputString.split(" ")[0].toLowerCase().equals("talk")) {
+
+            }
+
+            else if (inputString.split(" ")[0].toLowerCase().equals("interact")) {
+
+            }
+
         }
         catch (IllegalArgumentException e){
-            isValidString();
+            getUserInput();
         }
         System.out.println("Printed String: "+inputString);
         return inputString;
     }
 
-    public String getUserInput() {
-        return userInput;
+    private boolean inputValidation(String input) {
+        if (input.isEmpty() ) {
+            System.out.println("Your input cannot be empty");
+            return false;
+            //throw new IllegalArgumentException("You have not entered a move");
+        }
+        else if (input.matches("[-+]?[0-9]*\\.?[0-9]+")) {
+            System.out.println("You have entered an invalid move");
+            return false;
+        }
+        return true;
     }
+
 }
 
 

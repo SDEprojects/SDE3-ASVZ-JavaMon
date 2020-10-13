2afe3dc38ad909dc53c872b54293b86bde9620ca
yunyidi-han <yunyidi.han@gmail.com>
2020-10-13 13:04:50 -0700

    fixed some gameengine class merge conflicts

--- src/com/capstone/GameEngine.java
+++ src/com/capstone/GameEngine.java
@@ -40,6 +40,21 @@ public class GameEngine {
                 player1.checkMap();
             }
 
+            //for getting help menu (Fix input parser to allow for help as a single input later)
+            else if ((userInput.toLowerCase().equals("get help")) || (userInput.toLowerCase().equals("help"))) {
+                player1.showHelp();
+            }
+
+            //for checking your inventory
+            else if ((userInput.toLowerCase().equals("check bag")) || (userInput.toLowerCase().equals("check inventory"))) {
+                player1.checkInventory();
+            }
+
+            //for checking item use
+            else if (userInput.split(" ")[0].toLowerCase().equals("use")) {
+                String item = userInput.split(" ",2)[1].toLowerCase();
+                player1.useItem(item);
+            }
 
             //for the movement
             else if (userInput.split(" ")[0].toLowerCase().equals("go")) {
@@ -75,6 +90,41 @@ public class GameEngine {
             } else if (userInput.split(" ")[0].toLowerCase().equals("interact")) {
                 String interactable = userInput.split(" ",2)[1].toLowerCase();
                 if (player1.getCurrentRoom().getInteractableItem().toLowerCase().equals(interactable)) {
+
+                    //shop interface! Will probably move somewhere and make it a method so that it's not so CLUNKY
+                    if (interactable.equals("shop counter")) {
+                        System.out.println("--------PokeMart--------");
+                        System.out.println("Potion              $100");
+                        System.out.println("Super Potion        $500");
+                        System.out.println("Full Heal          $1000");
+                        System.out.println("Revive             $2500");
+                        System.out.println("------------------------");
+                        System.out.println("To purchase an item: buy <item>!");
+                        System.out.println("To exit shop: exit shop!");
+                        boolean exit = false;
+                        while (!exit) {
+                            String shopInput = parser.getUserInput().toLowerCase();
+                            if (shopInput.split(" ")[0].equals("buy")) {
+                                String item = shopInput.split(" ", 2)[1];
+                                switch (item) {
+                                    case "potion":
+                                        player1.buyItem("potion", 100);
+                                        break;
+                                    case "super potion":
+                                        player1.buyItem("super potion", 500);
+                                    case "full heal":
+                                        player1.buyItem("full heal", 1000);
+                                        break;
+                                    case "revive":
+                                        player1.buyItem("revive", 2500);
+                                        break;
+                                }
+                            } else if (shopInput.equals("exit shop")) {
+                                System.out.println("Thank you for your patronage!");
+                                exit = true;
+                            }
+                        }
+                    }
                     System.out.println("You try to interact with " + interactable);
                     System.out.println("We need to implement an interactables class <_>");
                 }
@@ -83,37 +133,6 @@ public class GameEngine {
             }
             System.out.println("=====================================================");
         }
-
-//        //everything below is hardcoded for testing
-//
-//        //for getting dialog of a npc, do these calls...
-//        System.out.println(game.listOfNpcs);
-//        System.out.println(game.npcDialog("Nurse Didi"));
-//
-//
-//        //must setCurrentRoom(room) first before calling move and other stuff)
-//
-//        System.out.println(player1.getName());
-//
-//
-//
-//        startingRoom.displayOutput();
-//
-//        //System.out.println(player1.getCurrentRoom()); //room class needs a toString to display itself... if needed, prob not needed
-//        System.out.println("===============================================");
-//
-//        if (player1.validMove("north")) {
-//            System.out.println("PLayer moved north");
-//            System.out.println("===============================================");
-//            Room roomNorth = game.getRoom(player1.getCurrentRoom().getNorthTile());
-//            player1.setCurrentRoom(roomNorth);
-//            roomNorth.displayOutput();
-//
-//
-//            //ASK TOM.... For implementation of NPCs in a Room... should we do a HAS-A relationship, or just query NPC Dialog when you interact as long as the current room contains the NPC.
-//
-//
-//        }
     }
     //Choose pokemon starter method.
     //TODO - complete the choose starter pokemon method.

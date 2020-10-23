db973c05657f3df026fa5b4ac518cb722f5a0a0a
helloamandamurphy <helloamandamurphy@gmail.com>
2020-10-23 15:32:06 -0700

    Write capabilities for log, but no reading yet

---
+++ src/com/capstone/LogTest.java
@@ -0,0 +1,13 @@
+package com.capstone;
+
+public class LogTest {
+
+
+    public static void main(String[] args) {
+        PlayerLog log = new PlayerLog();
+        log.updateLog("B");
+
+    }
+}
+
+
--- src/com/capstone/PlayerLog.java
+++ src/com/capstone/PlayerLog.java
@@ -1,7 +1,30 @@
 package com.capstone;
 
-import java.io.File;
+import java.io.*;
 
 public class PlayerLog {
-    private File log = new File("log.txt");
+    private File log = new File("src/com/capstone/log.txt");
+
+    public void updateLog(String action){
+        try {
+            FileReader fr = new FileReader(log);
+            FileWriter fw = new FileWriter(log, true);
+
+            BufferedReader br = new BufferedReader(fr);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            System.out.println("Trying to write");
+            bw.write(action);
+            bw.newLine();
+            bw.flush();
+            bw.close();
+
+//            System.out.println("Trying to read");
+//            br.read();
+        } catch (FileNotFoundException e){
+            System.out.println("The file was not found: " + e.getMessage());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
